3e8f822ca55032929c848f18186751cc3aaa0a75
VipinKumar <vk22914916@gmail.com>
2023-02-12 13:47:01 +0530

    first commit - complete project

---
+++ src/container/Chef/Chef.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import {images} from '../../constants';
+import { SubHeading } from '../../components';
+import './Chef.css';
+
+const Chef = () => (
+  <div className='app__bg app__wrapper section__padding'>
+    <div className='app__wrapper_img app__wrapper_img-reverse'>
+      <img src={images.chef} alt='chef'/>
+    </div>
+
+    <div className='app__wrapper_info'>
+      <SubHeading title="Chef's World"/>
+      <h1 className='headtext__cormorant'>What we believe in</h1>
+
+      <div className='app__chef-content'>
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote" />
+          <p className='p__opensans'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cil</p>
+        </div>
+        {/* <p className='p__opensans'>olore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p> */}
+      </div>
+
+     <div className='app__chef-sign'>
+      <p>Kevin Luo</p>
+      <p className='p__opensans'>Chef & Founder</p>
+      <img src={images.sign} alt="sign" />
+     </div>
+    </div>
+  </div>
+);
+
+export default Chef;
